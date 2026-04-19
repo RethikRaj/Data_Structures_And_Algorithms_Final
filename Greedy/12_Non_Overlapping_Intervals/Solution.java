9f767444119ac175fe08a5201208479b5866d125
RethikRaj <rethikrajrr@gmail.com>
2026-04-19 08:06:21 +0530

    added codes

--- Greedy/12_Non_Overlapping_Intervals/Solution.java
+++ Greedy/12_Non_Overlapping_Intervals/Solution.java
@@ -13,11 +13,8 @@ class Solution {
                     // Discard lastInterval
                     // Therfore new value lastInterval becomes interval[i]
                     lastInterval = intervals[i];
-                }else if(intervals[i][1] > lastInterval[1]) {
-                    // Discard intervals[i]
-                }else {
-                    // Keep whichever you like but discard other
                 }
+                // else : discard intervals[i] => no need to update lastInterval
 
                 ans += 1;
             }else {
@@ -29,7 +26,28 @@ class Solution {
         return ans;
     }
 
-    // Solution Two : Sort by end time
+    // Solution One Simplified code :
+    public int methodOneSimplified(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0]-b[0]); // sort by start time
+
+        int ans = 0; // ans denotes number of removals
+        int last_end_time = intervals[0][1];
+
+        for(int i = 1; i < intervals.length ;i++) {
+            if(intervals[i][0] < last_end_time) {
+                // Overlapping => Therefore we need to discard / remove any one of two intervals
+                // For minimum removals : remove the interval that ends later 
+                last_end_time = Math.min(last_end_time, intervals[i][1]);
+                ans += 1;
+            }else {
+                last_end_time = intervals[i][1];
+            }
+        }
+
+        return ans;
+    }
+
+    // Solution Two : Sort by end time ( R to L , tough - not intuituve)
     public int methodTwo(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]); // sort by end time
 
@@ -59,8 +77,31 @@ class Solution {
         return ans;
     }
 
+    // SolutionTwo Simplified (L to R)
+    public int methodTwoSimplified(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[1]-b[1]); // sort by end time
+
+        int prev_end_time = intervals[0][1];
+        int ans = 0;
+
+        for(int i = 1 ; i < intervals.length ; i++) {
+            if(intervals[i][0] < prev_end_time) {
+                // overlap => remove any one => here intervals[i][1] will always be >= prev_end_time => Remove intervals[i][1] => No need to update prev_end_time
+                ans += 1;
+            }else {
+                // No overlap 
+                prev_end_time = intervals[i][1];
+            }
+        }
+
+        return ans;
+    }
+
     public int eraseOverlapIntervals(int[][] intervals) {
         // return methodOne(intervals);
-        return methodTwo(intervals);
+        // return methodOneSimplified(intervals);
+
+        // return methodTwo(intervals);
+        return methodTwoSimplified(intervals);
     }
 }
