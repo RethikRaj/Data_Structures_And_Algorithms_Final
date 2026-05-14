c1c2aa34a04161d2e7f8231356493a50d0feb102
RethikRaj <rethikrajrr@gmail.com>
2026-05-14 10:58:03 +0530

    added codes

--- Tree/BinaryTree/20_Construct_Tree/Inorder_and_Postorder.java
+++ Tree/BinaryTree/20_Construct_Tree/Inorder_and_Postorder.java
@@ -1,32 +1,37 @@
 class Solution {
-    private int index;
-    // Reverse postorder
-    private TreeNode buildTreeUtil(int[] postorder, int start, int end, Map<Integer, Integer> inorderMap) {
-        if(index == -1) return null;
-        if(start > end) return null;
+    private int postorderIndex;
+
+    // Build tree using reverse postorder traversal 
+    private TreeNode buildTreeUtil(int[] postorder, int inorderStart, int inorderEnd, Map<Integer, Integer> inorderMap){
+        // Base cases:
+        if (postorderIndex == -1) return null;
+        if (inorderStart > inorderEnd) return null;
 
         // N
-        int element = postorder[index];
+        int element = postorder[postorderIndex];
         TreeNode root = new TreeNode(element);
-        int inorderIndex = inorderMap.get(element);
-        index -= 1;
+        postorderIndex--;
 
-        // R
-        root.right = buildTreeUtil(postorder, inorderIndex + 1, end, inorderMap);
-        // L
-        root.left = buildTreeUtil(postorder, start, inorderIndex - 1, inorderMap);
-        
+        // Find where this root sits in the inorder traversal
+        int inorderIndex = inorderMap.get(element);
+
+        // R - Elements to the right of inorderIndex in inorder belong to the right subtree
+        root.right = buildTreeUtil(postorder, inorderIndex + 1, inorderEnd, inorderMap);
+        // L - Elements to the left of inorderIndex in inorder belong to the left subtree
+        root.left = buildTreeUtil(postorder, inorderStart, inorderIndex - 1, inorderMap);
+
         return root;
     }
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // Map each value to its index in inorder for O(1) lookups
         Map<Integer, Integer> inorderMap = new HashMap<>();
-
-        for(int i = 0; i < inorder.length; i++) {
+        for (int i = 0; i < inorder.length; i++) {
             inorderMap.put(inorder[i], i);
         }
 
-        index = postorder.length - 1;
+        // Start from the last element of postorder (the root of the full tree)
+        postorderIndex = postorder.length - 1;
         return buildTreeUtil(postorder, 0, inorder.length - 1, inorderMap);
     }
 }
--- Tree/BinaryTree/20_Construct_Tree/Inorder_and_Preorder.java
+++ Tree/BinaryTree/20_Construct_Tree/Inorder_and_Preorder.java
@@ -1,30 +1,34 @@
 class Solution {
-    private int index;
-    // Preorder
-    private TreeNode buildTreeUtil(int[] preorder, int start, int end, Map<Integer, Integer> inorderMap) {
-        if(index == preorder.length) return null;
-        if(start > end) return null;
+    private int preorderIndex;
 
-        // N
-        int element = preorder[index];
+    private TreeNode buildTreeUtil(int[] preorder, int inorderStart, int inorderEnd, Map<Integer, Integer> inorderMap) {
+        // Base cases
+        if (preorderIndex == preorder.length) return null;
+        if (inorderStart > inorderEnd) return null;
+
+        
+        int element = preorder[preorderIndex];
         TreeNode root = new TreeNode(element);
+        preorderIndex++;
+        // Find where this element sits in the inorder traversal
         int inorderIndex = inorderMap.get(element);
-        index += 1;
-        // L
-        root.left = buildTreeUtil(preorder, start, inorderIndex - 1, inorderMap);
-        // R
-        root.right = buildTreeUtil(preorder, inorderIndex + 1, end, inorderMap);
+
+        // Elements to the left of inorderIndex in inorder belong to the left subtree
+        root.left = buildTreeUtil(preorder, inorderStart, inorderIndex - 1, inorderMap);
+        // Elements to the right of inorderIndex in inorder belong to the right subtree
+        root.right = buildTreeUtil(preorder, inorderIndex + 1, inorderEnd, inorderMap);
+
         return root;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // Map each value to its index in inorder for O(1) lookups
         Map<Integer, Integer> inorderMap = new HashMap<>();
-
-        for(int i = 0; i < inorder.length; i++) {
+        for (int i = 0; i < inorder.length; i++) {
             inorderMap.put(inorder[i], i);
         }
 
-        index = 0;
+        preorderIndex = 0;
         return buildTreeUtil(preorder, 0, inorder.length - 1, inorderMap);
     }
 }
